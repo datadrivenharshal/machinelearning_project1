e329bdf430ca7f5c949b726d223cee9f7f37b012
Harshal Pawar <datadrivenharshal@gmail.com>
2024-09-06 01:49:41 +0530

    logging and exception handling

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_details(error, error_detail:sys):
     _,_,exc_tb= error_detail.exc_info()
@@ -19,11 +19,11 @@ class CustomException(Exception):
         return self.error_message
 
 
-if __name__=="__main__":
+# if __name__=="__main__":
     
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divide by zero")
-        raise CustomException(e,sys)
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info("Divide by zero")
+#         raise CustomException(e,sys)
             
